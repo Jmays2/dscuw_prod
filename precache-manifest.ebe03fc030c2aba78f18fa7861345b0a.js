bb84eab55bdc44f13d43d8535725e7570cdb1376
Your Name <jmays2@wisc.edu>
2020-02-16 13:12:29 -0600

    Updates

---
+++ precache-manifest.ebe03fc030c2aba78f18fa7861345b0a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "339334f61e16b5196be3ec7c0fefe3a5",
+    "url": "/production/index.html"
+  },
+  {
+    "revision": "9d9d71da21e1e2f70525",
+    "url": "/production/static/css/main.1d53c003.chunk.css"
+  },
+  {
+    "revision": "6849a6af946c5753cbbf",
+    "url": "/production/static/js/2.65a6b92f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/production/static/js/2.65a6b92f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d9d71da21e1e2f70525",
+    "url": "/production/static/js/main.013fee19.chunk.js"
+  },
+  {
+    "revision": "94c40ccb2d38ab267af7",
+    "url": "/production/static/js/runtime-main.beb33fcb.js"
+  },
+  {
+    "revision": "003e486b5a7cc2c0dde7a71e2f9cdf44",
+    "url": "/production/static/media/DSCUWM_Logo.003e486b.png"
+  },
+  {
+    "revision": "12f0465352a62ed34d3f946a1e40297b",
+    "url": "/production/static/media/study_jams.12f04653.jpg"
+  },
+  {
+    "revision": "bbb5c4eb2bec455deae45b354917a564",
+    "url": "/production/static/media/tech_talks.bbb5c4eb.png"
+  }
+]);
